3f59c1c6b5120d9f8e34649dba48f1ee237d57db
Olek Pogorilyi <Olek.Pogorilyi@gmail.com>
2022-06-11 21:36:39 +1000

    added note

--- stor-item-demand-01.py
+++ stor-item-demand-01.py
@@ -22,7 +22,7 @@ import xgboost as xgb
 from fbprophet import Prophet
 
 
-PATH = "D:\Data\kaggle\demand-forecasting-kernels-only"
+PATH = "D:\Data\kaggle\demand-forecasting-kernels-only" #change path
 
 print(os.listdir(PATH))
 
